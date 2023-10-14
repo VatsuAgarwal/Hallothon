0fa7ec1383369399a185e045421160035b036c1a
Tanishq Yaduka <prabhatkyaduka@rediffmail.com>
2023-10-15 02:55:19 +0530

    Website changes - Tanishq

--- Website/hallothon/src/App.js
+++ Website/hallothon/src/App.js
@@ -19,6 +19,11 @@ const App = () => {
           zoom={15}
         >
           <Marker position={{ lat: 12.9353971, lng: 77.5355179 }} icon={"http://maps.google.com/mapfiles/ms/icons/green-dot.png"} />
+          <Marker position={{ lat: 12.9352056, lng: 77.5362696 }} icon={"http://maps.google.com/mapfiles/ms/icons/green-dot.png"} />
+          <Marker position={{ lat: 12.9353971, lng: 78.5355179 }} icon={"http://maps.google.com/mapfiles/ms/icons/green-dot.png"} />
+          <Marker position={{ lat: 12.9353971, lng: 79.5355179 }} icon={"http://maps.google.com/mapfiles/ms/icons/green-dot.png"} />
+          {/* <Marker position={{ lat: 12.9353971, lng:80.5355179 }} icon={"http://maps.google.com/mapfiles/ms/icons/green-dot.png"} /> */}
+          {/* <Marker position={{ lat: 12.9353971, lng: 81.5355179 }} icon={"http://maps.google.com/mapfiles/ms/icons/green-dot.png"} /> */}
         </GoogleMap>
       )}
     </div>
